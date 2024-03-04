91cd1edaed7b9f793a9a3965b9ac6039eb887a44
Mudassir-13 <mbaksh@hawk.iit.edu>
2024-03-03 23:00:38 -0600

    site.js
    adding javascript

---
+++ js/site.js
@@ -0,0 +1,4 @@
+document.getElementById('content').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Congratulations! You're one step closer to becoming a millionaire");
+  });
